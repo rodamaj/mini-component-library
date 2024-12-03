b50b33144e4d3fbd5cb61f0a1a1c00cbe773e1f4
Josue Amador <bjamadorr@gmail.com>
2024-12-02 19:12:27 -0600

    select video solution

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,79 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': '24px' }}>
+          <Icon id='chevron-down' strokeWidth={2} size={24}></Icon>
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+// const Wrapper = styled.select`
+//   background-color: ${COLORS.transparentGray15};
+//   color: ${COLORS.gray700};
+//   padding: 12px 16px;
+//   border-radius: 8px;
+//   border: none;
+//   appearance: none;
+//   width: fit-content;
+// `;
+
+// const StyledIcon = styled(Icon)`
+//   display: inline;
+//   position: absolute;
+//   right: 0;
+//   top: 0;
+// `;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
 
 export default Select;
